918b4e650ed4468083466ec853dbbe62fe3fca24
brtkn <newcreativedesingofficial@gmail.com>
2021-01-20 11:00:59 +0300

    add tempature/10

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,16 @@
-const ftoc = function() {
+const ftoc = function(givenNumber) {
+
+  const tempature = Math.round(((givenNumber - 32 ) * 5/9) * 10);
+  
+  return tempature/10;
 
 }
 
-const ctof = function() {
+const ctof = function(givenNumber) {
 
+    const tempature = Math.round((givenNumber * 9/5 + 32) * 10);
+
+    return tempature/10;
 }
 
 module.exports = {
