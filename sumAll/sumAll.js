444b244a6761183e44857689546d6d7a4d67da36
brtkn <newcreativedesingofficial@gmail.com>
2021-01-20 09:47:35 +0300

    recent fixes

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,21 +1,25 @@
 const sumAll = function(min, max) {
 
-    if (!Number.isInteger(min) || !Number.isInteger(max)) return 'ERROR';
-    if (min < 0 || max < 0 ) return 'ERROR';
-    
-    if(min > max){
-        const temp = min;
-        min = max;
-        max = temp; 
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+
+    if ( min < 0 || max < 0) return "ERROR";
+
+    if ( min > max) {
+
+        const temp = min ;
+        min = max ; 
+        max = temp ;
     }
 
     let sum = 0; 
-    for (let i = min; i < max ; i++){
+
+    for ( let i = min; i <= max ; i++) {
 
         sum += i;
-    }
 
-    return sum;
+    };
+
+    return sum ;
     
 };
 
