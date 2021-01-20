1a061f5ca12bdba46837afe81e50433deface74c
brtkn <newcreativedesingofficial@gmail.com>
2021-01-20 10:15:21 +0300

    leapYears with two conditions

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,4 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    const numYear = year;
+
+    if ( numYear % 4 == 0 && numYear % 400 == 0 ) {
+
+        return true;
+    } else if ( numYear % 4 == 0 && numYear % 100 !== 0) {
+
+        return true;
+    }else {
+
+        return false;
+    }
 
 }
 
