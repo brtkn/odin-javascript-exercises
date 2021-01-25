9c1909bc3c358a848c2cb5019132486fea38f361
brtkn <newcreativedesingofficial@gmail.com>
2021-01-25 09:55:39 +0300

    replacing multiply funtion and addind factorial

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,21 +1,31 @@
-function add () {
-	
+function add (a, b) {
+	return a + b;
 }
 
-function subtract () {
-	
+function subtract (a, b) {
+	return a - b;
 }
 
-function sum () {
-	
+function sum ([a,b]) {
+	let sum = a + b;
+	return sum;
 }
 
-function multiply () {
-	
+function multiply ([]) {
+	let sum = [...a * b] ;
+	return sum;
 }
 
-function power() {
-	
+function power(a, b) {
+	if(b === 0){
+		return 1;
+	}
+	else{
+		for(i = 0; i>= b; i++){
+			sum *= a[i];
+			let sum;
+		}
+	}return sum;
 }
 
 function factorial() {
